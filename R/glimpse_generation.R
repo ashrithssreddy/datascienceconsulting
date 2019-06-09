e07739e116b52c4a8bd0a1ee926ba310c14db845
Ashrith <ashrith@themathcompany.com>
2019-06-10 03:45:00 +0530

    Moved some files

--- R/glimpse_generation.R
+++ R/glimpse_generation.R
@@ -1,7 +1,8 @@
-rm(list=ls());cat("\014")
+# This function generates glimpse view of the data.frame passed into it
+# You are stringly advides to pass output_name to function, else it will create text file
 
-glimpse_generation = function(dataset, final_output_name = gsub(x = paste0("glimpse_",Sys.time(),".txt"),pattern = " |:|-",replacement = "_")){
-  sink(final_output_name, type=c("output"))  # Ensures the output is written to 01_glimpse.txt going forward
-  dplyr::glimpse(dataset)                   # Generates glimpse file
-  sink()                                    # Ensures the output is written to console going forward
+glimpse_generation <- function(dataset, output_name = gsub(x = paste0("glimpse_",Sys.time(),".txt"),pattern = " |:|-",replacement = "_")){
+  sink(output_name, type=c("output"))  # Ensures the output is written to 01_glimpse.txt going forward
+  dplyr::glimpse(dataset)              # Generates glimpse file
+  sink()                               # Ensures the output is written to console going forward
 }
