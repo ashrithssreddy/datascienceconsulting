32b9037b2f3a22f59214f8a025f5bdb269c98f99
Ssaiashrith Reddy - vn50ht9 <S.Sai.Ashrith.Reddy@walmart.com>
2020-03-06 17:20:43 +0530

    Fixed the documentation

--- R/01_glimpse.R
+++ R/01_glimpse.R
@@ -6,32 +6,25 @@
 #'
 #' @param dataset A data.frame
 #' @param output_filename Name of the output text file (should end in ".txt")
-#' Strongly advised to pass this parameter, else the function's default is "glimpse_timestamp.txt"
-#' @param output_directory Directory to write glimpse file to. Default is working directory
+#' Strongly advised to pass this parameter, else the function's default is "glimpse_<timestamp>.txt"
 #'
 #' @export
 #' @examples
-#'
-#' glimpse_to_file(dataset = mtcars, output_filename = "glimpse_mtcars.txt",
-#' output_directory = tempdir())
+#' glimpse_to_file(dataset = mtcars, output_filename = tempfile())
 
-glimpse_to_file <- function(dataset, output_filename = "", output_directory = getwd()){
-
+glimpse_to_file <- function(dataset, output_filename = ""){
   if(output_filename == ""){
     output_filename = gsub(x = paste0("glimpse_",Sys.time(),".txt"),pattern = " |:|-",replacement = "_")
   }
-  print(output_filename)
-  message("Writing file ",output_filename," to ",output_directory)
-  output_filename = paste0(output_directory, "\\", output_filename)
-  print(output_filename)
+  if(substr(output_filename,nchar(output_filename)-3, nchar(output_filename)) !=".txt"){
+    output_filename = paste0(output_filename, ".txt")
+  }
 
+  message("Writing glimpse of the dataset to ",output_filename)
+  sink(output_filename, type=c("output"), append = T)  # Ensures the glimpse output is written to output_filename text file
+  tibble::glimpse(dataset)                             # Generates and writes the output of tibble::glimpse() to output_filename text file
+  cat("\n");                                           # Blank line at the end of output
+  sink()                                               # Unmounts output_filename text file
 
-  sink(output_filename, type=c("output"),append = T)  # Ensures the glimpse output is written to output_filename text file
-  tibble::glimpse(dataset)                            # Generates and writes the output of tibble::glimpse() to output_filename text file
-  cat("\n");cat("\n")                                 # 2 blank lines at the end of output
-  sink()                                              # Unmounts output_filename text file
-
-  invisible()                                         # To return nothing
+  invisible()                                          # To return nothing
 }
-
-
